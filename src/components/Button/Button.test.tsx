60b8ec0f786ec3b043984128b04eeee709934f0c
Yasuhiro Ito <boccochan@gmail.com>
2024-01-30 22:35:45 +0900

    Add jest config

---
+++ src/components/Button/Button.test.tsx
@@ -0,0 +1,10 @@
+import { render } from "@testing-library/react";
+import React from "react";
+
+import { Button } from "./Button";
+
+describe("Button", () => {
+  it("This is sample test", async () => {
+    render(<Button />);
+  });
+});
