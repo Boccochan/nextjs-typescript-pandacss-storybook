59c97c717e3fc548d06b71570ae10d132dba01b4
Yasuhiro Ito <boccochan@gmail.com>
2024-02-02 08:27:16 +0900

    Remove comments

--- panda.config.ts
+++ panda.config.ts
@@ -3,17 +3,14 @@ import { defineConfig } from "@pandacss/dev";
 import { semanticTokens } from "@/styles";
 
 export default defineConfig({
-  // Whether to use css reset
   preflight: true,
 
-  // Where to look for your css declarations
   include: [
     "./src/**/*.{js,jsx,ts,tsx}",
     "./pages/**/*.{js,jsx,ts,tsx}",
     "./stories/**/*.{js,jsx,ts,tsx}",
   ],
 
-  // Files to exclude
   exclude: [],
   conditions: {
     osDark: process.env.STORYBOOK_DARK_MODE
@@ -21,13 +18,11 @@ export default defineConfig({
       : "@media (prefers-color-scheme: dark)",
   },
 
-  // Useful for theme customization
   theme: {
     extend: {
       semanticTokens,
     },
   },
 
-  // The output directory for your css system
   outdir: "styled-system",
 });
