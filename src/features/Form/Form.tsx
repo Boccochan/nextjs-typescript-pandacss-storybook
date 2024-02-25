7d2e90213262751cd9d939df921e9279553a5bc1
Yasuhiro Ito <boccochan@gmail.com>
2024-02-26 08:08:58 +0900

    Add zod

--- src/features/Form/Form.tsx
+++ src/features/Form/Form.tsx
@@ -1,19 +1,37 @@
 "use client";
+import { zodResolver } from "@hookform/resolvers/zod";
 import type { SubmitHandler } from "react-hook-form";
 import { useForm } from "react-hook-form";
+import { z } from "zod";
 
 import { Button } from "@/components/Button";
 import { Input } from "@/components/Input/Input";
 import { Select } from "@/components/Select";
 
-type IFormInput = {
-  country: string;
-  name: string;
-};
+export const basicFormSchema = z.object({
+  name: z.string().min(1, { message: "名前を入力してください" }),
+  country: z.string(),
+  // password: z
+  //   .string()
+  //   .min(8, { message: "8桁以上のパスワードを入力してください" })
+  //   .regex(/^[a-zA-Z0-9]+$/, {
+  //     message: "英大文字、英小文字、数字で入力してください",
+  //   }),
+});
+
+//https://stackoverflow.com/questions/77367244/a-solution-to-translating-zod-error-messages-using-next-intl
+export type BasicFormSchemaType = z.infer<typeof basicFormSchema>;
 
 export const Form = () => {
-  const { register, handleSubmit } = useForm<IFormInput>();
-  const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<BasicFormSchemaType>({
+    resolver: zodResolver(basicFormSchema),
+  });
+  const onSubmit: SubmitHandler<BasicFormSchemaType> = (data) =>
+    console.log(data);
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
@@ -25,6 +43,7 @@ export const Form = () => {
         ]}
       />
       <Input {...register("name")} width="md" />
+      {errors.name?.message}
       <Button label="Submit" type="submit" />
     </form>
   );
