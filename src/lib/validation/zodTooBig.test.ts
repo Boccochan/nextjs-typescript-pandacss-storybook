4d6a1b536ce5e0417d262eb720400ef6c6f522b9
Yasuhiro Ito <boccochan@gmail.com>
2024-03-17 18:33:15 +0900

    String too big validation

--- src/lib/validation/zodTooBig.test.ts
+++ src/lib/validation/zodTooBig.test.ts
@@ -72,7 +72,7 @@ describe("TooBig", () => {
 
     if (res.success === false) {
       expect(res.error.errors[0].message).toBe(
-        "配列は最大で1要素含む必要があります",
+        "配列は最大で1要素まで含むことができます",
       );
     }
   });
@@ -113,7 +113,7 @@ describe("TooBig", () => {
 
     if (res.success === false) {
       expect(res.error.errors[0].message).toBe(
-        "配列は最大で1要素含む必要があります",
+        "配列は最大で1要素まで含むことができます",
       );
     }
   });
@@ -164,6 +164,66 @@ describe("TooBig", () => {
     if (res.success === false) {
       expect(res.error.errors[0].message).toBe(
         "String must contain under 2 character(s)",
+      );
+    }
+  });
+
+  it("The too big string at most default error message in Japanese", async () => {
+    const t = await getTranslator("ja");
+    setI18nZodDefaultErrorMsg(t);
+
+    const val = z.string().max(1);
+
+    const res = val.safeParse("aa");
+
+    expect(res.success).toBeFalsy();
+
+    if (res.success === false) {
+      expect(res.error.errors[0].message).toBe(
+        "文字列は最大で1文字まで含むことができます",
+      );
+    }
+  });
+
+  it("The too big string exactly default error message in Japanese", async () => {
+    const t = await getTranslator("ja");
+    setI18nZodDefaultErrorMsg(t);
+
+    const val = z.string().length(2);
+
+    const res = val.safeParse("aaa");
+
+    expect(res.success).toBeFalsy();
+
+    if (res.success === false) {
+      expect(res.error.errors[0].message).toBe(
+        "文字列は2文字含む必要があります",
+      );
+    }
+  });
+
+  it("The too big string less than default error message in Japanese", async () => {
+    const t = await getTranslator("ja");
+    setI18nZodDefaultErrorMsg(t);
+
+    const val = z.string().superRefine((val, ctx) => {
+      if (val.length > 2) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.too_big,
+          maximum: 2,
+          type: "string",
+          inclusive: false,
+        });
+      }
+    });
+
+    const res = val.safeParse("aaa");
+
+    expect(res.success).toBeFalsy();
+
+    if (res.success === false) {
+      expect(res.error.errors[0].message).toBe(
+        "文字列は2要素未満である必要があります",
       );
     }
   });
