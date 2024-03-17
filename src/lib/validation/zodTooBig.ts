f647feef5e6c1e3d1e1c8f532718dd48531e7cd0
Yasuhiro Ito <boccochan@gmail.com>
2024-03-17 11:00:47 +0900

    Refactor too big error message

--- src/lib/validation/zodTooBig.ts
+++ src/lib/validation/zodTooBig.ts
@@ -4,17 +4,32 @@ import { AbstractHandler } from "../chainOrResponsibility";
 import type { Request, Response } from "./types";
 
 export class TooBig extends AbstractHandler<Request, Response> {
+  private createErrorMessage(request: Request, expectType: string) {
+    if (request.issue.code == ZodIssueCode.too_big) {
+      // For Typescript
+      if (request.issue.type === expectType) {
+        const max = request.issue.maximum.toString();
+        if (request.issue.exact) {
+          return {
+            message: request.t(`Too big ${expectType} exactly`, { max }),
+          };
+        } else if (request.issue.inclusive) {
+          return {
+            message: request.t(`Too big ${expectType} at most`, { max }),
+          };
+        } else {
+          return {
+            message: request.t(`Too big ${expectType} less than`, { max }),
+          };
+        }
+      }
+    }
+  }
+
   public handle(request: Request): Response | undefined {
     if (request.issue.code == ZodIssueCode.too_big) {
       if (request.issue.type === "array") {
-        const max = request.issue.maximum.toString();
-        if (request.issue.exact) {
-          return { message: request.t("Too big array exactly", { max }) };
-        } else if (request.issue.inclusive) {
-          return { message: request.t("Too big array at most", { max }) };
-        } else {
-          return { message: request.t("Too big array less than", { max }) };
-        }
+        return this.createErrorMessage(request, request.issue.type);
       }
       const message = request.t("Required");
 
