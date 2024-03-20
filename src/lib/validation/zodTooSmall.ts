7cac44e3b33e7153ce27ea176d4d55cc548947e1
Yasuhiro Ito <boccochan@gmail.com>
2024-03-20 10:57:47 +0900

    Support too min at least array validation

--- src/lib/validation/zodTooSmall.ts
+++ src/lib/validation/zodTooSmall.ts
@@ -4,11 +4,21 @@ import { AbstractHandler } from "../chainOrResponsibility";
 import type { Request, Response } from "./types";
 
 export class TooSmall extends AbstractHandler<Request, Response> {
+  private createErrorMessage(request: Request, expectType: string) {
+    if (request.issue.code == ZodIssueCode.too_small) {
+      const min = request.issue.minimum.toString();
+      return {
+        message: request.t(`Too small ${expectType} at least`, { min }),
+      };
+    }
+  }
   public handle(request: Request): Response | undefined {
     if (request.issue.code == ZodIssueCode.too_small) {
-      const message = request.t("hello");
+      if (["array"].includes(request.issue.type)) {
+        return this.createErrorMessage(request, request.issue.type);
+      }
 
-      // TODO: Add too small validation.
+      const message = request.t("hello");
 
       return { message };
     }
